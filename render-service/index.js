71232c44dba43700b8f1f3d1c2d7288e25ae1b51
connorklose12 <connorklose12@gmail.com>
2026-07-27 18:57:01 -0500

    adding sound fx and bg video p3

--- render-service/index.js
+++ render-service/index.js
@@ -188,7 +188,7 @@ async function renderClip({ bgVideo, bgStart, duration, imgPath, spritePath, aud
   const f = [];
   f.push(`[0:v]scale=${WIDTH}:${HEIGHT}:force_original_aspect_ratio=increase,crop=${WIDTH}:${HEIGHT}[bg]`);
   f.push(`[1:v]scale=${WIDTH - 160}:-1[postimg]`);
-  f.push(`[postimg]crop=w=iw:h='ih*min(1\\,t/${duration})':x=0:y=0:eval=frame[reveal]`);
+  f.push(`[postimg]drawbox=x=0:y='ih*min(1\\,t/${duration})':w=iw:h='ih*(1-min(1\\,t/${duration}))':color=black@1.0:t=fill[reveal]`);
   f.push(`[bg][reveal]overlay=80:200[withpost]`);
   let last = 'withpost';
   if (spritePath) {
